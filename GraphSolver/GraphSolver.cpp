cf33e33d3e6cc9fc3a0757a7b6a47f5ac5557e5f
Yash Malviya <yashmalviya98@gmail.com>
2021-06-12 16:54:33 +0530

    visitAllPaths

--- GraphSolver/GraphSolver.cpp
+++ GraphSolver/GraphSolver.cpp
@@ -5,6 +5,7 @@
 #include "SVF-FE/PAGBuilder.h"
 
 #include <unordered_set>
+#include <string>
 
 using namespace SVF;
 using namespace llvm;
@@ -12,22 +13,44 @@ using namespace std;
 
 static llvm::cl::opt<std::string> InputFilename(cl::Positional, llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
 
-void icfgDFS(NodeID id, ICFG* icfg, unordered_set<NodeID> visited) {
+void icfgDFS(ICFGNode* node, ICFG* icfg, unordered_set<NodeID> visited) {
+	return;
+}
+
+void visitAllPaths(ICFGNode* node, ICFG* icfg, unordered_set<NodeID>& visited, int depth) {
+	NodeID id = node->getId();
 	visited.insert(id);
-	ICFGNode* node = icfg->getICFGNode(id);
+	if (!node->hasOutgoingEdge()) {
+		SVFUtil::outs() << depth << "DEPTH: END POINT REACHED:" << node->toString() << "\n";
+	} else {
+		SVFUtil::outs() << depth << "DEPTH: " << node->toString() << "\n";
+	}
 
 	// Get Child Nodes
-	int numEdges = 0;
 	for(ICFGEdge* edge : node->getOutEdges()){
-		NodeID dst = edge->getDstID();
-		if !(visited.contains(id)) {
-			icfgDFS(dst, icfg, visited);
-			numEdges++;
+		ICFGNode* dst = edge->getDstNode();
+		NodeID dstID = dst->getId();
+		if (visited.find(dstID) == visited.end()) {
+			visitAllPaths(dst, icfg, visited, depth+1);
+		} 
+		// else {
+		// 	SVFUtil::outs() << "ALREADY VISITED: " << dstID << "\n";
+		// }
+	}
+	visited.erase(id);
+}
+
+void icfgFullDFS(ICFG* icfg) {
+	int dfsCounter = 1;
+	for(ICFG::iterator i = icfg->begin(); i != icfg->end(); i++) {
+		ICFGNode* node = i->second;
+		if (!node->hasIncomingEdge()) {
+			unordered_set<NodeID> visited;
+			SVFUtil::outs() << "ICFG DFS COUNTER: " << dfsCounter << "\n";
+			visitAllPaths(node, icfg, visited, 0);
+			dfsCounter++;
 		}
 	}
-	if (numEdges == 0) {
-
-	} 
 }
 
 int main(int argc, char ** argv) {
@@ -70,7 +93,7 @@ int main(int argc, char ** argv) {
 	//     }
 	// }
 
-	unordered_set<NodeID> visited;
+	icfgFullDFS(icfg);
 
 	return 0;
 }
