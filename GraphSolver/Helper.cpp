2977625b6f34f11bb4546b0b48a0017bea00d636
Yash Malviya <yashmalviya98@gmail.com>
2021-06-16 13:17:21 +0530

    Seperate helpers

---
+++ GraphSolver/Helper.cpp
@@ -0,0 +1,19 @@
+#include "helper.hpp"
+
+#include <vector>
+
+vector<ICFGNode*> getAllICFGStartPoints(ICFG* icfg) {
+	int counter = 1;
+	vector<ICFGNode*> startingPoints;
+	for(ICFG::iterator i = icfg->begin(); i != icfg->end(); i++) {
+		ICFGNode* node = i->second;
+		if (!node->hasIncomingEdge()) {
+			unordered_set<NodeID> visited;
+			// SVFUtil::outs() << "STARTPOINT COUNTER: " << counter << "\n";
+			startingPoints.push_back(node);
+			counter++;
+		}
+	}
+	return startingPoints;
+}
+
